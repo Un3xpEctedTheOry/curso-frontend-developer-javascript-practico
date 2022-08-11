c2ffb25cbbce248febc392f8ee922f0fc71b538f
Un3xpEctedThe0ry <monteromarrufoandremartin@gmail.com>
2022-08-10 21:50:48 -0500

    mobile menu fusion

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const mobileMenuBtn = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 navEmail.addEventListener('click', toggleDesktopMenu)
+mobileMenuBtn.addEventListener('click', toggleMobilepMenu)
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+function toggleDesktopMenu() {
+   desktopMenu.classList.toggle('inactive')
+}
+
+function toggleMobilepMenu() {
+   mobileMenu.classList.toggle('inactive')
 }
