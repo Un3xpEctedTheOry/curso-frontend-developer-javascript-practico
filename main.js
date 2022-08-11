db2d4a7012e15eb1759bb57d72d09ed844298e58
Un3xpEctedThe0ry <monteromarrufoandremartin@gmail.com>
2022-08-10 22:45:14 -0500

    shopping cart fusion

--- main.js
+++ main.js
@@ -2,14 +2,44 @@ const navEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const mobileMenuBtn = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
+const shopingCart = document.querySelector('.navbar-shopping-cart')
+const aside = document.querySelector('.product-detail')
 
 navEmail.addEventListener('click', toggleDesktopMenu)
-mobileMenuBtn.addEventListener('click', toggleMobilepMenu)
+mobileMenuBtn.addEventListener('click', toggleMobileMenu)
+shopingCart.addEventListener('click', toggleAside)
 
 function toggleDesktopMenu() {
+   const asideClosed = aside.classList.contains('inactive')
+
+   if (!asideClosed) {
+      aside.classList.toggle('inactive')
+   }
+
    desktopMenu.classList.toggle('inactive')
 }
 
-function toggleMobilepMenu() {
+function toggleMobileMenu() {
+   const asideClosed = aside.classList.contains('inactive')
+
+   if (!asideClosed) {
+      aside.classList.toggle('inactive')
+   }
+
    mobileMenu.classList.toggle('inactive')
+}
+
+function toggleAside() {
+   const mobileMenuClosed = mobileMenu.classList.contains('inactive')
+   const desktopMenuClosed = desktopMenu.classList.contains('inactive')
+
+   if (!mobileMenuClosed) {
+      toggleMobileMenu()
+   }
+
+   if (!desktopMenuClosed) {
+      toggleDesktopMenu()
+   }
+
+   aside.classList.toggle('inactive')
 }
